31c89eb25c5b36392123614b9e644e3a0b48cfd8
giorgi <khabazishvili20@gmail.com>
2024-09-03 04:27:05 +0400

    Logs for testing

--- src/js/requests/submitForm.js
+++ src/js/requests/submitForm.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const submitForm = async (formData) => {
     try {
         const response = await axios.post(import.meta.env.VITE_URL_API, formData);
+        console.log("submitted from :" + import.meta.env.VITE_URL_API)
         return response.data;
     } catch (error) {
         throw error; 
